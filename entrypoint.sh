9ba6bdefd8b261b5aabc3dc315c1f010218683b3
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2020-04-30 15:55:46 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+if [ "${GAME_AUTOUPDATE}" == "1" ]; then
+    ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +login anonymous +force_install_dir /home/container +app_update 1110390 +quit
+fi
+
+cp -r Extras/Rocket.Unturned Modules/
+
+cp -f linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamclient.so
+
+ulimit -n 2048
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Unturned_Headless_Data/Plugins/x86_64/
+
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+${MODIFIED_STARTUP}
